8a9f8e4eeade6400afb5daa1cd6cc62ccd2abcc5
lanyunzhu99 <zhulanyun@buaa.edu.cn>
2024-06-08 15:37:29 +0800

    .

--- select_images.py
+++ select_images.py
@@ -4,5 +4,5 @@ file_dir = '/mnt/1TBSSD/lanyunz/data/lerf_ovs/teatime_train/images'
 files = os.listdir(file_dir)
 for f in files:
     num = int(f[6:11])
-    if num // 10 == 0:
+    if num % 10 == 0:
         print(num)
